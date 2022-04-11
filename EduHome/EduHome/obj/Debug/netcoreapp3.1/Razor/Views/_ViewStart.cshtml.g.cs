01b7db94950c530d5137853050911c68e0fcdb80
ibrahim <ibrahimea@code.edu.az>
2022-04-11 11:44:06 +0400

    Feat : Front to back Home page and install all packeges

--- EduHome/EduHome/obj/Debug/netcoreapp3.1/Razor/Views/_ViewStart.cshtml.g.cs
+++ EduHome/EduHome/obj/Debug/netcoreapp3.1/Razor/Views/_ViewStart.cshtml.g.cs
@@ -19,15 +19,8 @@ using EduHome;
 #line default
 #line hidden
 #nullable disable
-#nullable restore
-#line 2 "C:\Users\HP\Desktop\Back-end Project\EduHome\EduHome\Views\_ViewImports.cshtml"
-using EduHome.Models;
-
-#line default
-#line hidden
-#nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"7091c65830b0329e613be026ede8a57552863778", @"/Views/_ViewStart.cshtml")]
-    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"24a0c480aceae0c1fc0430846b3b8389540ac9ad", @"/Views/_ViewImports.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"715add55a9f436dc17fe6b55f70d49aa4b812e6c", @"/Views/_ViewImports.cshtml")]
     public class Views__ViewStart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
